a61d6f087ab23942b213bfd081814a95a95a2fa1
hsh0517 <pelino0517@gmail.com>
2022-11-23 10:46:23 +0900

    [Client] Exploder 코드 작성

--- Client/Assets/Scripts/Contents/Exploder.cs
+++ Client/Assets/Scripts/Contents/Exploder.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Exploder : MonoBehaviour
 {
+    public float NextExplosionTime = 1f;
     public List<Explosion> Explosions = new List<Explosion>();
 
     private byte[,] explosionPattern =
@@ -59,17 +60,32 @@ public class Exploder : MonoBehaviour
             0, 1, 0, 1, 0
         },
     };
+    private WaitForSeconds nextExplosionTime;
 
     private void Start()
     {
         Managers.Object.Exploder = this;
+        nextExplosionTime = new WaitForSeconds(NextExplosionTime);
     }
 
     public void Explode(RepeatedField<int> patternIds)
     {
-        foreach (int patternId in patternIds)
+        StartCoroutine(Exploding(patternIds));
+    }
+
+    private IEnumerator Exploding(RepeatedField<int> patternIds)
+    {
+        for (int i = 0; i < patternIds.Count; i++)
         {
-            
+            for (int j = 0; j < Explosions.Count; j++)
+            {
+                if (explosionPattern[i, j] == 1)
+                {
+                    Explosions[j].gameObject.SetActive(true);
+                }
+            }
+
+            yield return nextExplosionTime;
         }
     }
 }
